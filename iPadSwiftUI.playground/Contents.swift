da91c25631649ef3991f9cbcd5274e980ed46b04
Rody Davis <rody.davis.jr@gmail.com>
2019-06-05 15:50:39 -0400

    adding ios playgound

---
+++ iPadSwiftUI.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: A UIKit based Playground for presenting user interface
+  
+import SwiftUI
+import PlaygroundSupport
+import UIKit
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World!")
+    }
+}
+
+//
+//class MyViewController : UIViewController {
+//    override func loadView() {
+//        let view = UIView()
+//        view.backgroundColor = .white
+//
+//        let label = UILabel()
+//        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
+//        label.text = "Hello World!"
+//        label.textColor = .black
+//
+//        view.addSubview(label)
+//        self.view = view
+//    }
+//}
+
+// Present the view controller in the Live View window
+PlaygroundPage.current.liveView = UIHostingController(rootView: ContentView())
